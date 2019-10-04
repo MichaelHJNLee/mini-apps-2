6fe061cca25e43e2d782be50db1c8d4d5440c90c
Michael Lee <michaellee@Michaels-MacBook-Pro-2.local>
2019-10-04 09:27:22 -0700

    completed

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,2 +1,88 @@
+import React from 'react';
+import { Line } from 'react-chartjs-2';
+import axios from 'axios';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+          {
+            label: 'BTC Prices',
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: 'rgba(75,192,192,0.4)',
+            borderColor: 'rgba(75,192,192,1)',
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: 'rgba(75,192,192,1)',
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: []
+          }
+        ]
+      }
+    }
+  }
 
+  componentDidMount() {
+    axios.get('https://api.coindesk.com/v1/bpi/historical/close.json')
+      .then((data) => {
+        let dates = [];
+        let prices = [];
+        for (let key in data.data.bpi) {
+          dates.push(key);
+          prices.push(data.data.bpi[key]);
+        }
+        this.setState({
+          data: {
+            labels: dates,
+            datasets: [
+              {
+                label: 'BTC Prices',
+                fill: false,
+                lineTension: 0.1,
+                backgroundColor: 'rgba(75,192,192,0.4)',
+                borderColor: 'rgba(75,192,192,1)',
+                borderCapStyle: 'butt',
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'miter',
+                pointBorderColor: 'rgba(75,192,192,1)',
+                pointBackgroundColor: '#fff',
+                pointBorderWidth: 1,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                pointHoverBorderColor: 'rgba(220,220,220,1)',
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                data: prices,
+              }
+            ]
+          }
+        })
+      })
+  }
+
+  render() {
+    return(
+      <div>
+        <Line data={this.state.data}/>
+        <div>"Powered by CoinDesk"</div>
+      </div>
+    )
+  }
+}
+
+export default App;
